1696638985579689918660873b665d0f53079e13
Rodrigo <rodrigo_pbro@hotmail.com>
2022-09-23 07:50:08 -0300

    adicionando eventos com js

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,55 @@
 
-const closeMenu = document.querySelector('.btn-menu')
-const openMenu = document.querySelector('.close')
+const openModal = document.querySelector('.btn-menu')
 
-closeMenu.addEventListener('click', function() {
-    closeMenu.classList.add('hide')
-    openMenu.classList.remove('hide')
+const closeModal = document.querySelector('.js-close-modal')
+
+const modal = document.getElementById('js-modal')
+
+function openMenu (){
+    modal.classList.add('active')
+}
+
+function closeMenu(){
+    modal.classList.toggle('active')
+}
+
+openModal.addEventListener('click', openMenu)
+closeModal.addEventListener('click', closeMenu)
+// modal.addEventListener('mouseleave' , () => {
+//     modal.classList.remove('active')
+// })
+
+
+
+
+// closeMenu.addEventListener('click', function() {
+//     closeMenu.classList.add('hide')
+//     openMenu.classList.remove('hide')
     
     
-})
+// })
 
-openMenu.addEventListener('click', function(){
-    openMenu.classList.add('hide')
-    closeMenu.classList.remove('hide')
-})
+// openMenu.addEventListener('click', function(){
+//     openMenu.classList.add('hide')
+//     closeMenu.classList.remove('hide')
+// })
 
 
+// =============== Menu Fixo ==============
+const header = document.getElementById('js-header')
+
+function menuFixo () {
+    if (window.pageYOffset > 80){
+        header.classList.add('fixed-menu')
+    } else {
+        header.classList.remove('fixed-menu')
+    }
+}
+
+document.addEventListener('scroll', menuFixo)
+
+
+// ============= SLIDE SWIPER ===========
 var swiper = new Swiper(".slide-noticias", {
     slidesPerView: 3.5,
     spaceBetween: 20,
@@ -49,3 +84,23 @@ AOS.init({
     duration: 1000,
     once: true
 });
+
+// ============ SCROLL SUAVE ==========
+const linksToSection = document.querySelectorAll('.js-nav li a')
+function scrollToSection (event) {
+    event.preventDefault()
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    const initPosition = section.offsetTop;
+    console.log(initPosition);
+
+    window.scrollTo({
+        top: initPosition,
+        behavior: "smooth"
+     })
+
+}
+
+linksToSection.forEach(links =>{
+    links.addEventListener('click', scrollToSection )
+})
